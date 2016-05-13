66ff431f99d358a51b812cf0650568117100f907
Oussama Jabbari <oussamax.jabbari@intel.com>
2016-05-13 17:21:04 +0200

    added fft

--- hello.py
+++ hello.py
@@ -18,7 +18,19 @@ def fe(x):
 def ftp(x):
     return np.exp(-abs(x))
 
+def fourier_ftp(f):
+    return (2/(1+4*(pi**2)*(f**2)))
+
+N=1000
 t = np.arange(-5,5,0.01)
-f = np.vectorize(ftp, otypes=[np.float])
-r = f(t)
-plt.plot(t, r)
+#f = np.vectorize(ftp, otypes=[np.float])
+y = ftp(t)
+f = np.fft.fftfreq(N, 0.01)
+f = np.fft.fftshift(f)
+fft_y = np.fft.fft(y, N)
+fft_y = np.fft.fftshift(fft_y)
+
+#plt.plot(t, y)
+#plt.plot(f[458:541], 1.0/N * np.abs(fft_y[458:541]))
+plt.plot(f, 1.0/N * np.abs(fft_y))
+#plt.plot(f[458:541], fourier_ftp(f[458:541]))
