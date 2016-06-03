20e8a2cc6c87c2794d5f66f4de6a8db60f49a4e1
Oussama Jabbari <oussamax.jabbari@intel.com>
2016-06-03 16:23:09 +0200

    add frequence shift

--- hello.py
+++ hello.py
@@ -20,17 +20,33 @@ def ftp(x):
 
 def fourier_ftp(f):
     return (2/(1+4*(pi**2)*(f**2)))
+    
+def decalage(x):
+    return ftp(x) * (np.exp(2*np.pi*5*x*1j))
 
-N=1000
-t = np.arange(-5,5,0.01)
+Fe=20
+Te=1.0/Fe
+Tstart=-5
+Tend=5
+N=int((Tend - Tstart) / Te)
+t = np.arange(Tstart,Tend, Te)
 #f = np.vectorize(ftp, otypes=[np.float])
 y = ftp(t)
-f = np.fft.fftfreq(N, 0.01)
+y_decal = decalage(t)
+f = np.fft.fftfreq(N, Te)
 f = np.fft.fftshift(f)
 fft_y = np.fft.fft(y, N)
 fft_y = np.fft.fftshift(fft_y)
 
+fft_y_decal = np.fft.fft(y_decal, N)
+fft_y_decal = np.fft.fftshift(fft_y_decal)
+
+xt = np.abs(np.fft.ifft(fft_y))
+
 #plt.plot(t, y)
+#plt.plot(t, xt)
 #plt.plot(f[458:541], 1.0/N * np.abs(fft_y[458:541]))
-plt.plot(f, 1.0/N * np.abs(fft_y))
+#plt.plot(f, Te * np.abs(fft_y))
+plt.plot(f, Te * np.angle(fft_y_decal))
+#plt.plot(t, fourier_ftp(t))
 #plt.plot(f[458:541], fourier_ftp(f[458:541]))
